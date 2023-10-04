8afb4e4b3cd64d174ca6ad143fe2345eadbe7c4d
casual6603 <135954286+casual6603@users.noreply.github.com>
2023-10-04 11:05:17 -0700

    Update Ui.py
    Added image buttons, made ui partially work for just adding / removing tasks.

--- Gameify Your Life/Ui.py
+++ Gameify Your Life/Ui.py
@@ -1,38 +1,90 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter
+from tkinter import Scrollbar
+import tkinter.messagebox
+import pickle
+from tkinter import * 
+from tkinter.ttk import *
+
+
+ 
+
+def add_task():
+    task = entry_task.get()
+    if task != "":
+        listbox_tasks.insert(tkinter.END, task)
+        entry_task.delete(0, tkinter.END)
+    else:
+        tkinter.messagebox.showwarning(title="Warning!", message="You must enter a task.")
+
+def delete_task():
+    try:
+        task_index = listbox_tasks.curselection()[0]
+        listbox_tasks.delete(task_index)
+    except:
+        tkinter.messagebox.showwarning(title="Warning!", message="You must select a task.")
+
+def load_tasks():
+    try:
+        tasks = pickle.load(open("tasks.dat", "rb"))
+        listbox_tasks.delete(0, tkinter.END)
+        for task in tasks:
+            listbox_tasks.insert(tkinter.END, task)
+    except:
+        tkinter.messagebox.showwarning(title="Warning!", message="Cannot find tasks.dat.")
+
+def save_tasks():
+    tasks = listbox_tasks.get(0, listbox_tasks.size())
+    pickle.dump(tasks, open("tasks.dat", "wb"))
+ 
+ 
+ 
+ 
+ 
+ 
  
  
 # Main tkinter window
-root = Tk()
+root = tkinter.Tk()
 root.geometry("960x540")
-root.configure(background='gray24')
- 
- 
+root.configure(background='gray14')
+root.title("To-Do List")
 
- 
-# right section
-right_frame = Frame(root, width=250, height=540, bg='gray15')
-right_frame.grid(row=0, column=1)
+#creating the photo icons that are used in the bottom left of the screen to add / remove / save / items
+photo = PhotoImage (name= 'realrx', file= 'pic.png', height= 20, width= 202)
+plus = PhotoImage (name= 'rel', file= 'plus(50x50).png', height= 20, width= 20)
+
+# Create GUI
+frame_tasks = tkinter.Frame(root, width=200, height=540, bg='black')
+frame_tasks.place(x=0,y=0)
+
+listbox_tasks = tkinter.Listbox(root, height=200, width=22, bg='grey18')
+listbox_tasks.place(x=0,y=0)
+
+entry_task = tkinter.Entry(root, width=21)
+entry_task.place(x=0, y= 485)
+
+button_add_task = tkinter.Button(root, image=plus, command=add_task)
+button_add_task.place(x=0, y= 512)
+
+#button_delete_task = tkinter.Button(root, text="real", command=delete_task, bg='black')
+#button_delete_task.place(x = 10, y = 500)
+
+#button_load_tasks = tkinter.Button(root, text="Load tasks", width=22, command=load_tasks)
+#button_load_tasks.place(x=0, y= 470)
+
+#button_save_tasks = tkinter.Button(root, text="Save tasks", width=22, command=save_tasks)
+#button_save_tasks.place(x=0, y= 500)
 
 
-# left section 
-left_frame = Frame(root, width=710, height=540)
-left_frame.grid(row=10, column=15)
-left_frame.configure(background='gray10')
-
-
-# text
-top_label = Label(right_frame,
-                        text ='To do List',
-                        background="gray15",
-                        fg= "white",
-                        font=("Helvetica",18)
-                        )
-top_label.place(x=50, y=-4)
 
 
 
 
-mainloop()
+
+
+root.mainloop()
+
+
+
 
 
