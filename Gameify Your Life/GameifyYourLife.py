57438f2276142afbbffc2fe429e3e6ccdce6b944
casual6603 <malak.john111@gmail.com>
2023-10-06 17:57:14 -0700

    Cleaning up Functions

--- Gameify Your Life/GameifyYourLife.py
+++ Gameify Your Life/GameifyYourLife.py
@@ -6,6 +6,13 @@ root = Tk()
 root.geometry("960x540")
 root.configure(background='gray14')
 root.title("To-Do List")
+root.resizable = (False, False)
+
+c = Canvas(width= 250, height=480, bg = 'Black')
+c.xview_scroll(number= 10, what=['units'])
+c.place(x = 0, y = 0)
+
+
 
 tasks = []
 y_offset = 0
@@ -14,13 +21,13 @@ def add_task():
     global y_offset
     task = entry_task.get()
     if task != "":
-        task_frame = Frame(root)
-        checkbutton = Checkbutton(task_frame, text=task)
+        task_frame = Frame(c)
+        checkbutton = Checkbutton(task_frame, text=task, bg= 'black')
         checkbutton.pack(side=LEFT)
-        remove_button = Button(task_frame, text='x', command=lambda t=task: remove_task(t))
+        remove_button = Button(task_frame, text='x', command=lambda t=task: remove_task(t), bg='black')
         remove_button.pack(side=LEFT)
-        task_frame.place(x=10, y=y_offset)
-        y_offset += 20
+        task_frame.place(x=0, y=y_offset)
+        y_offset += 30
         tasks.append({"task": task, "checkbutton": checkbutton, "remove_button": remove_button})
     else:
         showwarning("Warning!", "You must enter a task.")
@@ -34,7 +41,7 @@ def remove_task(task_name):
             tasks.remove(item)
             break
 
-entry_task = Entry(root, width=21)
+entry_task = Entry(root, width=26)
 entry_task.place(x=0, y=485)
 
 plus_icon = PhotoImage(file='Gameify Your Life/plus(50x50).png')
